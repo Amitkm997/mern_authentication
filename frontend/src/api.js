5eb5fe8942ab71bedffdeb123e3e0b9725e4cb8d
Amitkm997 <amitkm@997gmail.com>
2025-05-21 13:42:17 +0530

    deploying on versal

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
+const API = axios.create({ baseURL: process.env.REACT_APP_API_URL }); // adjust if needed
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
