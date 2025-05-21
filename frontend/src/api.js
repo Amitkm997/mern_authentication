2245259b950bf5dffe1f574e7ceecad17a9ae210
Amitkm997 <amitkm@997gmail.com>
2025-05-21 14:34:01 +0530

    added react-touter-dom

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-import dotenv from "dotenv";
-dotenv.config();
+
 const API = axios.create({ baseURL: process.env.REACT_APP_API_URL }); // adjust if needed
 
 API.interceptors.request.use((req) => {
